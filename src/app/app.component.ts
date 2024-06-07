dab050fb1f2450378305512d3e5d050d2beda3c0
ibvservices0 <ibvservices0@gmail.com>
2024-06-07 13:27:50 +0200

    siguiente31

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+import { Platform } from '@angular/cdk/platform';
+import { HostListener } from '@angular/core';
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +11,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'camerafeet0';
+
+  constructor(private platform: Platform) {
+    if (this.platform.ANDROID) {
+      console.log("platform.ANDROID");
+      //this.lockOrientation();
+    }
+    else if (this.platform.IOS){
+      console.log("platform.IOS");
+      //this.lockOrientation();
+    }
+  }
+
+  /*
+  lockOrientation() {
+    if (screen.orientation?.lock) {screen.orientation.lock('landscape');}
+  }
+  */
+
+  /*
+  @HostListener('window:orientationchange', ['$event'])
+  onOrientationChange(event: Event) {
+    event.preventDefault();
+  }
+  */
+
+
 }
--- src/app/screen01/screen01.component.ts
+++ src/app/screen01/screen01.component.ts
@@ -1,9 +1,11 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { from, fromEvent, Observable, throwError, Subscription } from 'rxjs';
-
 import { Router } from '@angular/router';
-
 import { KernelfeetService } from '../kernelfeet.service';
+
+
+import { Platform } from '@angular/cdk/platform';
+import { HostListener } from '@angular/core';
 
 
 @Component({
@@ -13,7 +15,17 @@ import { KernelfeetService } from '../kernelfeet.service';
 })
 export class Screen01Component implements OnInit, OnDestroy {
 
-  constructor(private router: Router, public kernelfeet_service: KernelfeetService){}
+  private isAndroid: boolean = false;
+  private isIos: boolean = false;
+  private isSafari: boolean = false;
+  public isPortrait: boolean = true;
+
+
+  constructor(private router: Router, public kernelfeet_service: KernelfeetService, private platform: Platform){
+    if (this.platform.ANDROID) {this.isAndroid = true;}
+    else if (this.platform.IOS){this.isIos = true;}
+    else if (this.platform.SAFARI){this.isSafari = true;}
+  }
 
   ngOnInit(){}
 
@@ -23,6 +35,28 @@ export class Screen01Component implements OnInit, OnDestroy {
   public toScreen02() {
     console.log("TAGG::Screen01::toScreen02");
     this.router.navigateByUrl('/screen02');
+  }
+
+  @HostListener('window:orientationchange', ['$event'])
+  onOrientationChange(event: Event) {
+    if (this.isAndroid){
+      if (screen.orientation.type.includes('portrait')){this.isPortrait = true;}
+      else if (screen.orientation.type.includes('landscape')){this.isPortrait = false;}
+      else {this.isPortrait = true;}
+    }
+    else if (this.isIos){
+      //orientation-Changed-iPHONE
+      if (window.innerHeight > window.innerWidth){this.isPortrait = false;}
+      else {this.isPortrait = true;}
+    }
+    else if (this.isSafari){
+      //orientation-Changed-iPAD
+      if (window.innerHeight > window.innerWidth){this.isPortrait = false;}
+      else {this.isPortrait = true;}
+    }
+    else{
+      console.log('orientation-Changed');
+    }
   }
 
 }
