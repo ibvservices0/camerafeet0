8883ffdee1f8da058e442bb22f07ca773e13faf3
ibvservices0 <ibvservices0@gmail.com>
2024-06-10 14:58:58 +0200

    siguiente39

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
 
-import { Platform } from '@angular/cdk/platform';
-import { HostListener } from '@angular/core';
 
 
 @Component({
@@ -12,29 +10,8 @@ import { HostListener } from '@angular/core';
 export class AppComponent {
   title = 'camerafeet0';
 
-  constructor(private platform: Platform) {
-    if (this.platform.ANDROID) {
-      console.log("platform.ANDROID");
-      //this.lockOrientation();
-    }
-    else if (this.platform.IOS){
-      console.log("platform.IOS");
-      //this.lockOrientation();
-    }
+  constructor() {
   }
-
-  /*
-  lockOrientation() {
-    if (screen.orientation?.lock) {screen.orientation.lock('landscape');}
-  }
-  */
-
-  /*
-  @HostListener('window:orientationchange', ['$event'])
-  onOrientationChange(event: Event) {
-    event.preventDefault();
-  }
-  */
 
 
 }
--- src/app/screen02/screen02.component.ts
+++ src/app/screen02/screen02.component.ts
@@ -5,6 +5,10 @@ import { Router } from '@angular/router';
 
 import { KernelfeetService } from '../kernelfeet.service';
 
+//import * as mytest from '../../scripts/mytest.js'
+
+
+
 @Component({
   selector: 'app-screen02',
   templateUrl: './screen02.component.html',
@@ -12,14 +16,36 @@ import { KernelfeetService } from '../kernelfeet.service';
 })
 export class Screen02Component implements OnInit, OnDestroy {
 
+  //private dato: any;
+  private divAutorizando: any;
+  private divAutorizadoOk: any;
+  private divAutorizadoError: any;
+
+
+
   constructor(private router: Router, public kernelfeet_service: KernelfeetService){}
 
-  ngOnInit(){}
+  ngOnInit(){
+    //mytest.tester();
+    this.divAutorizando = document.getElementById("div_autorizando");
+    this.divAutorizadoOk = document.getElementById("div_autorizado_ok");
+    this.divAutorizadoError = document.getElementById("div_autorizado_error");
+    this.divAutorizando.removeAttribute("hidden");
+    this.divAutorizadoOk.setAttribute("hidden", "hidden");
+    this.divAutorizadoError.setAttribute("hidden", "hidden");
+  }
 
   ngOnDestroy(){}
 
-  public actionScreen02() {
-    console.log("TAGG::Screen02::actionScreen02");
+
+  public actionScreen02left() {
+    console.log("TAGG::Screen02::actionScreen02left");
+    alert('Left');
+  }
+
+  public actionScreen02right() {
+    console.log("TAGG::Screen02::actionScreen02right");
+    alert('Right');
   }
 
 }
