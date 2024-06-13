b2eec07602a0812a40fbbe354f6fcafdfb045d4e
ibvservices0 <ibvservices0@gmail.com>
2024-06-13 11:58:01 +0200

    Auto-generated commit

--- scripts-FE3J5FVA.js
+++
@@ -1,1 +0,0 @@
-export function tester(){alert("Hello")}export function permission_accelerometers(){DeviceMotionEvent&&typeof DeviceMotionEvent.requestPermission=="function"&&DeviceMotionEvent.requestPermission()}
